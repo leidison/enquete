1c1e2a5a5409c2b0b52bc99fdb11fdce75ababc6
Leidison Siqueira Barbosa <leidison.sb@gmail.com>
2016-04-05 10:44:31 -0300

    Construção inicial do projeto. Já adicionei os bundles para REST, provedor de usuário e OAuth2

---
+++ api/README.md
@@ -0,0 +1,4 @@
+api
+===
+
+A Symfony project created on April 5, 2016, 10:28 am.
---
+++ api/app/AppKernel.php
@@ -0,0 +1,41 @@
+<?php
+
+use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\Config\Loader\LoaderInterface;
+
+class AppKernel extends Kernel
+{
+    public function registerBundles()
+    {
+        $bundles = array(
+            new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+            new Symfony\Bundle\SecurityBundle\SecurityBundle(),
+            new Symfony\Bundle\TwigBundle\TwigBundle(),
+            new Symfony\Bundle\MonologBundle\MonologBundle(),
+            new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
+            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
+            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new AppBundle\AppBundle(),
+            # REST
+            new FOS\RestBundle\FOSRestBundle(),
+            # provedor de usuário
+            new FOS\UserBundle\FOSUserBundle(),
+            # autenticação via OAuth2
+            new FOS\OAuthServerBundle\FOSOAuthServerBundle(),
+        );
+
+        if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
+            $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
+            $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
+            $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
+            $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+        }
+
+        return $bundles;
+    }
+
+    public function registerContainerConfiguration(LoaderInterface $loader)
+    {
+        $loader->load($this->getRootDir() . '/config/config_' . $this->getEnvironment() . '.yml');
+    }
+}
